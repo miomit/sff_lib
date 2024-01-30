5bb9f84da65bdafd47f95229521fea0d240ae400
Yura Movsesyan <movsesyan-03@inbox.ru>
2024-01-30 21:40:33 +0300

    added Stream

--- lib/src/dir.dart
+++ lib/src/dir.dart
@@ -4,7 +4,7 @@ import 'package:path/path.dart' show basename;
 /// Recursively copying a file with its contents
 ///
 /// If a file with the same name exists, no copying will be performed.
-Future<void> copyDirRec(Directory dirIn, Directory dirOut) async {
+Stream<(File, File)> copyDirRec(Directory dirIn, Directory dirOut) async* {
   if (!dirOut.existsSync()) {
     dirOut.createSync();
   }
@@ -12,28 +12,32 @@ Future<void> copyDirRec(Directory dirIn, Directory dirOut) async {
   await for (final entitie in dirIn.list()) {
     if (entitie.statSync().type == FileSystemEntityType.file) {
       if (!File("${dirOut.path}/${entitie.uri.pathSegments.last}").existsSync()) {
-        File(entitie.path).copySync("${dirOut.path}/${entitie.uri.pathSegments.last}");
+        final file = File(entitie.path);
+        final fileCopy = file.copySync("${dirOut.path}/${entitie.uri.pathSegments.last}");
+        yield (file, fileCopy);
       }
     }
     else if (entitie.statSync().type == FileSystemEntityType.directory) {
-      await copyDirRec(
+      final stream = copyDirRec(
         Directory("${dirIn.path}/${basename(entitie.path)}"),
         Directory("${dirOut.path}/${basename(entitie.path)}")
       );
+
+      await for (final log in stream) {
+        yield log;
+      }
     }
   }
 }
 
 /// recursively copies the contents from the first directory 
 /// to another, and then in the same way but on the reverse
-void syncDir(Directory dir1, Directory dir2) async {
-  await copyDirRec(
-    dir1,
-    dir2
-  );
+Stream<(File, File)> syncDir(Directory dir1, Directory dir2) async* {
+  await for (final log in copyDirRec(dir1, dir2)) {
+    yield log;
+  }
 
-  await copyDirRec(
-    dir2,
-    dir1
-  );
+  await for (final log in copyDirRec(dir2, dir1)) {
+    yield log;
+  }
 }
