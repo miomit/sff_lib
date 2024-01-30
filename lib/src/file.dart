3ae697f4ff4391dac3e42afd522659678d03fb11
Yura Movsesyan <movsesyan-03@inbox.ru>
2024-01-30 14:49:43 +0300

    added findDuplicates

--- lib/src/file.dart
+++ lib/src/file.dart
@@ -40,3 +40,29 @@ Future<bool> compareFilesEquality(File file1, File file2) async {
 
   return true;
 }
+
+/// Stream the channel that transmits the original file and its duplicate
+Stream<(File, File)> findDuplicates(Directory dir) async* {
+  
+  List<File> files = [];
+
+  await for (final entitie in dir.list( recursive: true)) {
+    if (entitie.statSync().type == FileSystemEntityType.file) {
+      final file = File(entitie.path);
+      var isDuplicat = false;
+
+      // TODO реализовать бинарный поиск
+      for (final f in files) {
+        if (await compareFilesEquality(f, file)) {
+          yield (f, file);
+          isDuplicat = true;
+          break;
+        }
+      }
+
+      if (!isDuplicat) {
+        files.add(file);
+      }
+    }
+  }
+}
