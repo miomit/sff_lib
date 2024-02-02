faeb4348b4cad2b0c1ba50305beecae4c94c9541
Yura Movsesyan <movsesyan-03@inbox.ru>
2024-02-02 09:50:17 +0300

    update example

--- example/findDuplicates.dart
+++ example/findDuplicates.dart
@@ -4,7 +4,7 @@ import 'package:path/path.dart' as p;
 import 'package:sff_lib/sff_lib.dart';
 
 void main() {
-  findDuplicates(Directory('/home/yura'), recursive: false, filter: (path) {
+  findDuplicates([Directory('/home/yura/Pictures'), Directory('/home/yura/Documents')], recursive: true, filter: (path) {
     return p.extension(path) == ".jpg";
   }).listen((event) {
     print("\n------\n${event.$1}\n${event.$2}");
