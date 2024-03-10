ae391d6127ca1b7acb44111e76e324d118ea485b
Yura Movsesyan <movsesyan-03@inbox.ru>
2024-03-10 22:31:49 +0400

    refactoring func compareFilesEquality

--- lib/src/file.dart
+++ lib/src/file.dart
@@ -5,41 +5,7 @@ import 'package:convert/convert.dart' show AccumulatorSink;
 
 /// comparing two files for identity by content
 Future<bool> compareFilesEquality(File file1, File file2) async {
-
-  if (file1.statSync().size != file2.statSync().size) {
-    return false;
-  }
-
-  // help function for comparing two lists for identity
-  listEqual(List<int> l1, List<int> l2) {
-    if (l1.length != l2.length) {
-      return false;
-    }
-
-    for (int i = 0; i < l1.length; i++){
-      if (l1[i] != l2[i]){
-        return false;
-      }
-    }
-
-    return true;
-  }
-
-  var a = false;
-  List<int> buff = const[];
-
-  await for(final val in StreamGroup.merge([file1.openRead(), file2.openRead()])) {
-    if (a = !a) {
-      buff = val;
-      continue;
-    }
-
-    if (!listEqual(buff, val)) {
-      return false;
-    }
-  }
-
-  return true;
+  return (await generateHashFile(file1)) == (await generateHashFile(file2));
 }
 
 /// Stream the channel that transmits ([File], [File]) the original file and its duplicate
