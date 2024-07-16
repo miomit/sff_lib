40c98af53a244883808b286d50725c4446753cd6
Yura Movsesyan <movsesyan-03@inbox.ru>
2024-07-16 19:51:23 +0300

    added types and getType

--- lib/src/file_details.dart
+++ lib/src/file_details.dart
@@ -4,7 +4,49 @@ import 'package:path/path.dart' as p;
 enum FileType {
   document,
   picture,
-  music,
+  vector,
+  audio,
+  video;
+
+  static FileType? getType(String ext) => switch (ext) {
+        '.jpeg' ||
+        '.jpg' ||
+        '.png' ||
+        '.webp' ||
+        '.gif' ||
+        '.raw' ||
+        '.tiff' ||
+        '.psd' => //
+          FileType.picture,
+        '.svg' ||
+        '.esp' ||
+        '.pdf' ||
+        '.ai' ||
+        '.cdr' => //
+          FileType.vector,
+        '.avi' ||
+        '.mkv' ||
+        '.mp4' ||
+        '.mpeg' ||
+        '.ogv' ||
+        '.webm' => //
+          FileType.video,
+        '.m4a' ||
+        '.mp3' ||
+        '.wav' ||
+        '.ogg' ||
+        '.mpa' ||
+        '.flac' => //
+          FileType.audio,
+        '.pdf' ||
+        '.djvy' ||
+        '.doc' ||
+        '.docx' ||
+        '.txt' ||
+        '.md' => //
+          FileType.document,
+        _ => null,
+      };
 }
 
 class FileDetails {
@@ -15,15 +57,16 @@ class FileDetails {
 
   final String dirPath;
 
-  final FileType? type;
+  FileType? type;
 
   File get file => File(path);
   bool get isExist => file.existsSync();
 
   FileDetails({
     required this.path,
-    this.type,
   })  : name = p.basenameWithoutExtension(path),
         extension = p.extension(path),
-        dirPath = p.dirname(path);
+        dirPath = p.dirname(path) {
+    type = FileType.getType(extension);
+  }
 }
