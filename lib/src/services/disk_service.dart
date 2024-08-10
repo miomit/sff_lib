f2986074819edddd8ab6d932c5780a9302b66f6e
Yura Movsesyan <movsesyan-03@inbox.ru>
2024-08-10 11:04:45 +0300

    made DiskService static

--- lib/src/services/disk_service.dart
+++ lib/src/services/disk_service.dart
@@ -3,55 +3,31 @@ import 'package:path/path.dart';
 import 'package:sff_lib/services.dart';
 import 'package:sff_lib/src/errors/io_error.dart';
 
-/// Virtual disk to work with [IFilesystemService].
-class DiskService implements IDiskService, IIOService {
-  final Map<String, IFilesystemService> _disks = {};
+/// Mechanism to set up a link to [IFilesystemService].
+class DiskService {
+  static final Map<String, IFilesystemService> _disks = {};
 
-  Result<IFilesystemService, IOError> getFsByPath(String path) {
+  static Result<IFilesystemEntityService, IOError> open(String path) {
     return switch (_disks[rootPrefix(path)]) {
-      IFilesystemService fs => Ok(fs),
+      IFilesystemService fs => fs.find(path),
       _ => Err(IOError.doesNotExist),
     };
   }
 
-  @override
-  List<String> getAllTarget() => _disks.keys.toList();
+  /// Return the name of all connected [IFilesystemService].
+  static List<String> getAllTarget() => _disks.keys.toList();
 
-  @override
-  Result<(), IOError> mount(IFilesystemService fs) {
+  /// Establishing a connection to [IFilesystemService], otherwise returns error [IOError].
+  static Result<(), IOError> mount(IFilesystemService fs) {
     if (_disks[fs.rootPath] == null) {
       _disks[fs.rootPath] = fs;
-      return fs.connect(this);
+      return fs.connect();
     }
 
     return Err(IOError.dirExist);
   }
 
-  @override
-  Result<IFilesystemEntityService, IOError> open(String path) {
-    if (_disks[rootPrefix(path)] case IFilesystemService fs) {
-      return fs.find(path);
-    }
-
-    return Err(IOError.doesNotExist);
-  }
-
-  @override
-  Result<IFilesystemEntityService, IOError> create(
-    String path,
-    FilesystemEntityTypeService type,
-  ) {
-    if (_disks[rootPrefix(path)] case IFilesystemService fs) {
-      return switch (type) {
-        FilesystemEntityTypeService.dir => fs.mkdir(path),
-        FilesystemEntityTypeService.file => fs.touch(path),
-      };
-    }
-
-    return Err(IOError.doesNotExist);
-  }
-
-  @override
+  /// Disables connection to [IFilesystemService], otherwise returns error [IOError].
   Result<(), IOError> umount(String target) {
     if (_disks["$target:\\"] case IFilesystemService fs) {
       fs.disconnect();
@@ -60,48 +36,5 @@ class DiskService implements IDiskService, IIOService {
     }
 
     return Err(IOError.doesNotExist);
-  }
-
-  @override
-  Result<IFilesystemEntityService, IOError> copy(
-    String pathEntityIn,
-    String pathDirOut,
-  ) {
-    final entity = open(pathEntityIn);
-
-    return entity.and(open(pathDirOut)).andThen(
-      (ent) {
-        if (ent is IDirService) return Ok(ent);
-        return Err(IOError.doesNotExist);
-      },
-    ).andThen(
-      (dir) => getFsByPath(dir.path).andThen(
-        (fs) => fs.copy(entity.unwrap(), dir),
-      ),
-    );
-  }
-
-  @override
-  Result<(), IOError> delete(String path) {
-    return getFsByPath(path).andThen((fs) => fs.delete(path));
-  }
-
-  @override
-  Result<IFilesystemEntityService, IOError> move(
-    String pathEntity,
-    String pathDir,
-  ) {
-    final entity = open(pathEntity);
-
-    return entity.and(open(pathDir)).andThen(
-      (ent) {
-        if (ent is IDirService) return Ok(ent);
-        return Err(IOError.doesNotExist);
-      },
-    ).andThen(
-      (dir) => getFsByPath(dir.path).andThen(
-        (fs) => fs.move(entity.unwrap(), dir),
-      ),
-    );
   }
 }
