39f8e03edf152017846aa0f26f96cdfd5aa5b046
Yura Movsesyan <movsesyan-03@inbox.ru>
2024-08-11 14:18:16 +0300

    rm Result and added getFilesystemByTarget

--- lib/src/services/disk_service.dart
+++ lib/src/services/disk_service.dart
@@ -1,32 +1,37 @@
-import 'package:option_result/result.dart';
 import 'package:sff_lib/services.dart';
-import 'package:sff_lib/src/errors/io_error.dart';
 
 /// Mechanism to set up a link to [IFilesystemService].
 class DiskService {
   static final Map<String, IFilesystemService> _disks = {};
 
-  /// Return the name of all connected [IFilesystemService].
-  static List<String> getAllTarget() => _disks.keys.toList();
+  /// Return the [IFilesystemService] by target
+  static IFilesystemService? getFilesystemByTarget(String target) {
+    return _disks[target];
+  }
 
-  /// Establishing a connection to [IFilesystemService], otherwise returns error [IOError].
-  static Result<(), IOError> mount(IFilesystemService fs) {
+  /// Return the name of all connected [IFilesystemService].
+  static List<String> getAllTarget() {
+    return _disks.keys.toList();
+  }
+
+  /// Establishing a connection to [IFilesystemService].
+  static bool mount(IFilesystemService fs) {
     if (_disks[fs.rootPath] == null) {
       _disks[fs.rootPath] = fs;
       return fs.connect();
     }
 
-    return Err(IOError.dirExist);
+    return false;
   }
 
-  /// Disables connection to [IFilesystemService], otherwise returns error [IOError].
-  Result<(), IOError> umount(String target) {
-    if (_disks["$target:\\"] case IFilesystemService fs) {
+  /// Disables connection to [IFilesystemService].
+  bool umount(String target) {
+    if (_disks[target] case IFilesystemService fs) {
       fs.disconnect();
       _disks.remove("$target:\\");
-      return Ok(());
+      return true;
     }
 
-    return Err(IOError.doesNotExist);
+    return false;
   }
 }
