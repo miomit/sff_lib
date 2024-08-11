af3e084b78799de71ddb7bda3aa4283abe18d66a
Yura Movsesyan <movsesyan-03@inbox.ru>
2024-08-11 20:54:01 +0300

    added copy, create, delete and openRead

--- lib/src/services/file_service.dart
+++ lib/src/services/file_service.dart
@@ -1,1 +1,21 @@
-class FileService {}
+import 'package:sff_lib/services.dart';
+
+class FileService extends FilesystemEntityService {
+  FileService(super.path);
+
+  FileService copy(String newPath) {
+    return fs.copy(path, newPath);
+  }
+
+  FileService create({bool recursive = false}) {
+    return fs.touch(path, recursive: recursive);
+  }
+
+  bool delete({bool recursive = false}) {
+    return fs.delete(path, recursive: recursive);
+  }
+
+  Stream<List<int>> openRead() async* {
+    yield* fs.openRead(path);
+  }
+}
