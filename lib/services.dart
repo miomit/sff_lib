d35a7541725110622bea26e9d156a9527d4ba248
Yura Movsesyan <movsesyan-03@inbox.ru>
2024-08-11 20:24:03 +0300

    init new service

--- lib/services.dart
+++ lib/services.dart
@@ -1,24 +1,15 @@
 // Copyright (C) 2024 Yura Movsesyan
 library;
 
-export 'src/contracts/services/i_file_service.dart';
-export 'src/contracts/services/i_dir_service.dart';
-export 'src/contracts/services/i_stat_service.dart';
-export 'src/contracts/services/i_stat_file_service.dart';
-export 'src/contracts/services/i_stat_dir_service.dart';
-export 'src/contracts/services/i_disk_service.dart';
 export 'src/contracts/services/i_io_service.dart';
 export 'src/contracts/services/i_filesystem_service.dart';
-export 'src/contracts/services/i_filesystem_entity_service.dart';
 
+export 'src/services/dir_service.dart';
+export 'src/services/file_service.dart';
+export 'src/services/stat_service.dart';
+export 'src/services/filesystem_entity_service.dart';
 export 'src/services/disk_service.dart';
 export 'src/services/filesystem_entity_type_service.dart';
-
-export 'src/services/dirs/virtual_dir_service.dart';
-export 'src/services/dirs/native_dir_service.dart';
-
-export 'src/services/files/virtual_file_service.dart';
-export 'src/services/files/native_file_service.dart';
 
 export 'src/services/filesystems/virtual_filesystem_service.dart';
 export 'src/services/filesystems/native_filesystem_service.dart';
