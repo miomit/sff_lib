05edc8636cea62e27e1af2aafe7230330008c77b
Yura Movsesyan <movsesyan-03@inbox.ru>
2024-08-14 16:09:13 +0300

    added export disk

--- lib/filesystem.dart
+++ lib/filesystem.dart
@@ -9,6 +9,8 @@ export 'src/contract/filesystem/ifilesystem.dart';
 export 'src/filesystem/entity.dart';
 export 'src/filesystem/entity_type.dart';
 
+export 'src/filesystem/disk.dart';
+
 export 'src/filesystem/native.dart';
 export 'src/filesystem/virtual.dart';
 
