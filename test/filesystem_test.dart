6384fa644ec45aac95cd9200f8b61ac378fff1eb
Yura Movsesyan <movsesyan-03@inbox.ru>
2024-08-14 16:53:57 +0300

    added tests for Disk\getFileSystemAndRelativePathByPath\Without fs

--- test/filesystem_test.dart
+++ test/filesystem_test.dart
@@ -5,15 +5,68 @@ void main() {
   group('Disk', () {
     group('getFileSystemAndRelativePathByPath', () {
       group('Without fs', () {
-        test(r"'' -> (null, null)", () {});
+        test(r"'' -> (null, null)", () {
+          expect(
+            Disk.io.getFileSystemAndRelativePathByPath(
+              r"",
+            ),
+            equals((null, null)),
+          );
+        });
 
-        test(r"F:\ -> (null, '\')", () {});
+        test(r"'\' -> (null, null)", () {
+          expect(
+            Disk.io.getFileSystemAndRelativePathByPath(
+              r"",
+            ),
+            equals((null, null)),
+          );
+        });
 
-        test(r"F:\user_ -> (null, '\user_')", () {});
+        test(r"'\path\to\file' -> (null, null)", () {
+          expect(
+            Disk.io.getFileSystemAndRelativePathByPath(
+              r"",
+            ),
+            equals((null, null)),
+          );
+        });
 
-        test(r"F:\user_\ -> (null, '\user_')", () {});
+        test(r"F:\ -> (null, '\')", () {
+          expect(
+            Disk.io.getFileSystemAndRelativePathByPath(
+              r"F:\",
+            ),
+            equals((null, r'\')),
+          );
+        });
 
-        test(r"USB:\img\info.png -> (null, '\img\info.png')", () {});
+        test(r"F:\user_ -> (null, '\user_')", () {
+          expect(
+            Disk.io.getFileSystemAndRelativePathByPath(
+              r"F:\user_",
+            ),
+            equals((null, r'\user_')),
+          );
+        });
+
+        test(r"F:\user_\ -> (null, '\user_')", () {
+          expect(
+            Disk.io.getFileSystemAndRelativePathByPath(
+              r"F:\user_\",
+            ),
+            equals((null, r'\user_')),
+          );
+        });
+
+        test(r"USB:\img\info.png -> (null, '\img\info.png')", () {
+          expect(
+            Disk.io.getFileSystemAndRelativePathByPath(
+              r"USB:\img\info.png",
+            ),
+            equals((null, r'\img\info.png')),
+          );
+        });
       });
     });
   });
