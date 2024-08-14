a1d053a5f19c4e6aedc36d1065d3c11e7e75d2a1
Yura Movsesyan <movsesyan-03@inbox.ru>
2024-08-14 19:06:55 +0300

    init dir and file

---
+++ lib/src/filesystem/dir.dart
@@ -0,0 +1,5 @@
+import 'package:sff_lib/filesystem.dart';
+
+class Dir extends Entity {
+  Dir(super.path);
+}
---
+++ lib/src/filesystem/file.dart
@@ -0,0 +1,5 @@
+import 'package:sff_lib/filesystem.dart';
+
+class File extends Entity {
+  File(super.path);
+}
