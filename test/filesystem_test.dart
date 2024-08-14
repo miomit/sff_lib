7178796a102214e128cfbacd1ebab686aed87c9a
Yura Movsesyan <movsesyan-03@inbox.ru>
2024-08-14 18:19:00 +0300

    added test for getFileSystemAndRelativePathByPath With fs

--- test/filesystem_test.dart
+++ test/filesystem_test.dart
@@ -68,6 +68,81 @@ void main() {
           );
         });
       });
+
+      group("With fs", () {
+        final aFS = Virtual();
+        final bFS = Native();
+        final cFS = Virtual();
+
+        setUp(() {
+          Disk.io.mount(r"A:\", aFS);
+          Disk.io.mount(r"B:\", bFS);
+          Disk.io.mount(r"C:\", cFS);
+        });
+
+        test(r"'' -> (null, null)", () {
+          expect(
+            Disk.io.getFileSystemAndRelativePathByPath(
+              r"",
+            ),
+            equals((null, null)),
+          );
+        });
+
+        test(r"'\' -> (null, null)", () {
+          expect(
+            Disk.io.getFileSystemAndRelativePathByPath(
+              r"",
+            ),
+            equals((null, null)),
+          );
+        });
+
+        test(r"'\path\to\file' -> (null, null)", () {
+          expect(
+            Disk.io.getFileSystemAndRelativePathByPath(
+              r"",
+            ),
+            equals((null, null)),
+          );
+        });
+
+        test(r"A:\ -> (a fs, '\')", () {
+          expect(
+            Disk.io.getFileSystemAndRelativePathByPath(
+              r"A:\",
+            ),
+            equals((aFS, r'\')),
+          );
+        });
+
+        test(r"B:\user_ -> (b fs, '\user_')", () {
+          expect(
+            Disk.io.getFileSystemAndRelativePathByPath(
+              r"B:\user_",
+            ),
+            equals((bFS, r'\user_')),
+          );
+        });
+
+        test(r"C:\user_\ -> (c fs, '\user_')", () {
+          expect(
+            Disk.io.getFileSystemAndRelativePathByPath(
+              r"C:\user_\",
+            ),
+            equals((cFS, r'\user_')),
+          );
+        });
+
+        test(r"U:\img\info.png -> (null, '\img\info.png')", () {
+          expect(
+            Disk.io.getFileSystemAndRelativePathByPath(
+              r"U:\img\info.png",
+            ),
+            equals((null, r'\img\info.png')),
+          );
+        });
+      });
     });
   });
 
