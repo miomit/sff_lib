6ee9bc58ac85963c898ab454aba83e834270647b
Yura Movsesyan <movsesyan-03@inbox.ru>
2024-08-14 16:07:52 +0300

    init test for filesystem

---
+++ test/filesystem_test.dart
@@ -0,0 +1,15 @@
+import 'package:test/test.dart';
+
+void main() {
+  group('Disk', () {});
+
+  group('Virtual', () {});
+
+  group('Native', () {});
+
+  group('Entity', () {});
+
+  group('Dir', () {});
+
+  group('File', () {});
+}
