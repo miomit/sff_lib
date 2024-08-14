740e7e995cec28744a954408735d147dfd335836
Yura Movsesyan <movsesyan-03@inbox.ru>
2024-08-14 16:19:06 +0300

    rm null for relative path

--- test/filesystem_test.dart
+++ test/filesystem_test.dart
@@ -1,7 +1,20 @@
+import 'package:sff_lib/filesystem.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('Disk', () {});
+  group('Disk', () {
+    test("getFileSystemAndRelativePathByPath", () {
+      var disk = Disk();
+
+      final path1 = r"C:\";
+      final path2 = r"C:\user_\";
+      final path3 = r"C:\Users\user_\OneDrive\Рабочий стол\sff_lib";
+
+      final (fs, rPath) = disk.getFileSystemAndRelativePathByPath(path1);
+      expect(fs, equals(null));
+      expect(rPath, equals(null));
+    });
+  });
 
   group('Virtual', () {});
 
