4b36d35493482e93e77e928219c7bc9b0d195748
Yura Movsesyan <movsesyan-03@inbox.ru>
2024-08-14 18:04:11 +0300

    init native and virtual filesystem

--- lib/src/filesystem/virtual.dart
+++ lib/src/filesystem/virtual.dart
@@ -1,1 +1,64 @@
+import 'package:sff_lib/filesystem.dart';
 
+class Virtual implements IFileSystem {
+  @override
+  void connect() {
+    // TODO: implement connect
+  }
+
+  @override
+  void disconnect() {
+    // TODO: implement disconnect
+  }
+
+  @override
+  Entity? open(String path) {
+    // TODO: implement open
+    throw UnimplementedError();
+  }
+
+  @override
+  void create(
+    String path, {
+    bool recursive = false,
+    EntityType type = EntityType.file,
+  }) {
+    // TODO: implement create
+  }
+
+  @override
+  Entity copy(String filePath, String dirPath) {
+    // TODO: implement copy
+    throw UnimplementedError();
+  }
+
+  @override
+  Entity move(String pathIn, String pathOut) {
+    // TODO: implement move
+    throw UnimplementedError();
+  }
+
+  @override
+  bool delete(String path, {bool recursive = false}) {
+    // TODO: implement delete
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream<Entity> list(String dirPath) {
+    // TODO: implement list
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream<int> openRead(String filePath) {
+    // TODO: implement openRead
+    throw UnimplementedError();
+  }
+
+  @override
+  Stat stat(String path) {
+    // TODO: implement stat
+    throw UnimplementedError();
+  }
+}
