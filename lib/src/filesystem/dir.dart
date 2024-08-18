21c521b2d96bcf95a653719c24971a01bc76ba29
Yura Movsesyan <movsesyan-03@inbox.ru>
2024-08-18 17:47:08 +0300

    added onException

--- lib/src/filesystem/dir.dart
+++ lib/src/filesystem/dir.dart
@@ -3,11 +3,22 @@ import 'package:sff_lib/filesystem.dart';
 class Dir extends Entity {
   Dir(String path, {Disk? io}) : super(path, type: EntityType.dir, io: io);
 
-  Stream<Entity> list({bool recursive = false}) async* {
+  Stream<Entity> list({
+    bool recursive = false,
+    void Function(Exception e, Entity entity)? onException,
+  }) async* {
     await for (final entity in io.list(path)) {
-      yield entity;
-      if (recursive && entity is Dir) {
-        yield* entity.list(recursive: true);
+      try {
+        yield entity;
+        if (recursive && entity is Dir) {
+          yield* entity.list(recursive: true);
+        }
+      } on Exception catch (e) {
+        if (onException != null) {
+          onException(e, entity);
+        } else {
+          rethrow;
+        }
       }
     }
   }
