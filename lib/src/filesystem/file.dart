c15f5844778b2f1002612eac6263c13afeaa36b4
Yura Movsesyan <movsesyan-03@inbox.ru>
2024-08-18 18:17:23 +0300

    added generateHash

--- lib/src/filesystem/file.dart
+++ lib/src/filesystem/file.dart
@@ -1,3 +1,5 @@
+import 'package:convert/convert.dart';
+import 'package:crypto/crypto.dart';
 import 'package:sff_lib/filesystem.dart';
 
 class File extends Entity {
@@ -10,4 +12,19 @@ class File extends Entity {
   File copy(String path) => io.copy(super.path, path);
 
   Stream<List<int>> openRead() => io.openRead(path);
+
+  /// Method that generates a hash code from the contents of a file.
+  Future<Digest> generateHash({
+    Hash hashMethod = sha1,
+  }) async {
+    var output = AccumulatorSink<Digest>();
+    var input = hashMethod.startChunkedConversion(output);
+
+    await for (final val in openRead()) {
+      input.add(val);
+    }
+
+    input.close();
+    return output.events.single;
+  }
 }
