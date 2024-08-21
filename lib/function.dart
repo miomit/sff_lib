5e865de6016f0c7ee4f6b793d200cb0c46820b54
Yura Movsesyan <movsesyan-03@inbox.ru>
2024-08-21 10:47:39 +0300

    export ifunction

--- lib/function.dart
+++ lib/function.dart
@@ -1,6 +1,8 @@
 // Copyright (C) 2024 Yura Movsesyan
 library;
 
+export 'src/contract/function/ifunction.dart';
+
 export 'src/function/copy_dir_rec.dart';
 export 'src/function/sync_dir.dart';
 export 'src/function/find_duplicates.dart';
